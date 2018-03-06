5bc73ee936884ca2adaff9effe18799b59b18ac5
Cam Carter <ccprimitivo@gmail.com>
2018-03-06 22:36:34 +0000

    Added process class... hopefully this works maybe

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,7 @@
-print("This is a test!")
+class Process(object):
+    def __init__(self, arrival_time, total_cpu_time, cpu_burst, cpu_wait, pid):
+        self.arrival_time = arrival_time
+        self.total_cpu_time = total_cpu_time
+        self.cpu_burst = cpu_burst
+        self.cpu_wait = cpu_wait
+        slef.pid = pid

1b52d194b71ccbc438705479fcd27fe5d55ad7a0
Cam Carter <ccprimitivo@gmail.com>
2018-03-13 22:57:19 +0000

    Too many changes to count

--- scheduler.py
+++ scheduler.py
@@ -4,45 +4,72 @@ from collections import deque
 from process import Process
 
 number_of_processes = 3
-process_list = deque([])
-ready = Queue(maxsize=1)
-process1 = Process()
-process1.create('process1.txt')
+ready_queue = Queue()
+wait_queue = Queue()
+done = Queue()
 
-pid = process1.pid
-# process arrival time
-process_arrival_time = 5
-# process burst time
-process_burst = 10
-
-burst_time = process1.get_burst()
-# Defines a test variable for burst time to use in queue
-# burst_time = ready.get()
+process = {}
+for i in range(1, number_of_processes + 1):
+    process[i] = Process()
+    process[i].create('processes/process%d.txt' % (i))
+    ready_queue.put(process[i])
 
 # system clock
 sysclock = 0
 time_quantum = 5
 time_quantum_temp = 0
 # infinite loop, breaks when process_burst is decremented to 0
+wait_time = None
+
 i = 1
 while True:
+    sysclock += 1
+    if i > number_of_processes:
+        break
+
+    pid = process[i].pid
+    arrival_time = process[i].arrival_time
+     
+    if i > 1 and process[i - 1].wait_times:
+        wait_time = process[i - 1].get_wait()
+    if wait_time != None:
+        wait_time -= 1
+        if wait_time == 0:
+            ready_queue.put(wait_queue.get())
+
+    if process[i].burst_times:
+        burst_time = process[i].get_burst()
+    else:
+        i += 1
+        sysclock += 1
+        continue
+
+    if process[i].burst_times and process[i].wait_times is None:
+        done.put(process[i])
+        i += 1
+        sysclock += 1
+        continue
+
     # checks process arrival time against the system clock
-    if sysclock >= process_arrival_time:
+    if sysclock >= arrival_time:
         burst_time -= 1
         time_quantum -= 1
 
     # decrements time_quantum
     # increments system clock
-    sysclock += 1
     
     # checks it process_burst has reached zero and breaks the loop
     if burst_time == 0:
-        break
+        wait_queue.put(process[i])
+        i += 1
 
     # performs a context switch once the time_quantum reaches 0
     if time_quantum == 0:
         sysclock += 5
         time_quantum = 5
+    
+    if ready_queue.empty() and wait_queue.empty():
+        break
 
 # prints termination status of process and the amount of time it took to execute
 print('Process ' + repr(pid) + ' has been terminated.')

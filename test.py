1f6f15dbee887a204913a5655675cf4b3334c18a
Cam Carter <ccprimitivo@gmail.com>
2018-03-14 21:18:41 +0000

    running_process declaration is working

--- test.py
+++ test.py
@@ -4,8 +4,15 @@ from src.process import Process
 
 ready_queue = Queue(maxsize=3)
 process = {}
+
 number_of_processes = 3
 sysclock = 0
+
+burst_time = 0
+wait_time = 0
+
+running_process = None
+waiting_process = Process()
 
 for i in range(1, number_of_processes + 1):
     process[i] = Process()
@@ -16,7 +23,14 @@ while True:
         if sysclock == process[i].arrival_time:
             ready_queue.put(process[i])
             print('Process ' + repr(process[i].pid) + ' has arrived.')
+    
+    if running_process is None:
+        try:
+            running_process = ready_queue.get(False)
+            print('Currently running process: ' + repr(running_process.pid))
+        except Empty:
+            pass
 
     sysclock += 1
-    if ready_queue.full() is True:
-        break
+
+

8e77054a035cfa31eb828985c41484713fe3c94a
Cam Carter <ccprimitivo@gmail.com>
2018-03-14 22:09:35 +0000

    Here you go Todd

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ number_of_processes = 3
 sysclock = 0
 
 burst_time = 0
+is_declared = False
 wait_time = 0
 
 running_process = None
@@ -31,6 +32,35 @@ while True:
         except Empty:
             pass
 
+    if wait_process is None:
+        try:
+            waiting_process
+
+    if running_process != None:
+        if burst_time == 0 and is_declared is False:
+                burst_time = running_process.get_burst()
+                is_declared = True
+                print(burst_time)
+        elif: burst_time == 0 and is_declared is True:
+            wait_queue.put(running_process)
+            sysclock += 5
+            burst_time = 0
+            is_decalred = False
+            running_process = None
+        elif time_quantum == 0:
+            running_process.burst_times.appendleft(burst_time)
+            ready_queue.put(running_process)
+            sysclock += 5
+            burst_time = 0
+            is_decalred = False
+            running_process = None
+            time_quantum = 5
+        else:
+            bust_time -= 1
+            time_quantum -= 1
+
+
+            
     sysclock += 1
 
 

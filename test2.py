b103b7e467e0f3cb42be946e4ed58c047b3892fb
Cam Carter <ccprimitivo@gmail.com>
2018-03-15 11:02:03 +0000

    Working hard or hardly working?

--- test2.py
+++ test2.py
@@ -14,10 +14,14 @@ for i in range(1, number_of_processes + 1):
     process[i] = Process()
     process[i].create('processes/process%d.txt' % i )
 
+print(process[1].pid)
+print(process[2].pid)
+print(process[3].pid)
+
 wait_time = 0
 time_quantum = 5
-burst_time = 5
-sysclock = 0
+burst_time = 4
+sysclock = 1
 
 running_process = Process()
 waiting_process = Process()
@@ -26,17 +30,14 @@ j = 0
 while True:
     # WORKS
     # adds processes to the ready_queue at there specified arrival_time
-    for i in range(i, number_of_processes + 1):
+    for i in range(1, number_of_processes + 1):
         if sysclock == process[i].arrival_time:
             ready_queue.put(process[i])
             print('Process ' + repr(process[i].pid) + ' has arrived.')
-    try:
+    try: 
         running_process = ready_queue.get(False)
     except Empty:
         pass
-
-    if running_process.burst_times:
-        burst_time = running_process.get_burst()
 
     while True:
         time_quantum -= 1
@@ -44,19 +45,18 @@ while True:
 
         if burst_time == 0:
             wait_queue.put(running_process)
-            try:
-                running_process = ready_queue.get(False)
-            except Empty:
-                pass
             sysclock += 5
             print('test')
+            burst_time = 4
+
             break
         elif time_quantum == 0:
+            ready_queue.put(running_process)
+            running_process.burst_times.appendleft(burst_time)
             time_quantum = 5
             sysclock += 5
             break
 
     sysclock += 1
     j += 1
-    if i == 10:
-        break
+    print(sysclock)

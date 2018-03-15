5f715f7241237779027e39585fa809322c7253a4
Cam Carter <ccprimitivo@gmail.com>
2018-03-15 13:06:11 +0000

    Here you go todd

--- test2.py
+++ test2.py
@@ -14,14 +14,13 @@ for i in range(1, number_of_processes + 1):
     process[i] = Process()
     process[i].create('processes/process%d.txt' % i )
 
-print(process[1].pid)
-print(process[2].pid)
-print(process[3].pid)
-
 wait_time = 0
 time_quantum = 5
 burst_time = 4
 sysclock = 1
+terminated = 0
+context_switch = False
+switch_counter = 1
 
 running_process = Process()
 waiting_process = Process()
@@ -33,30 +32,48 @@ while True:
     for i in range(1, number_of_processes + 1):
         if sysclock == process[i].arrival_time:
             ready_queue.put(process[i])
-            print('Process ' + repr(process[i].pid) + ' has arrived.')
-    try: 
-        running_process = ready_queue.get(False)
-    except Empty:
-        pass
+            print('Process ' + repr(process[i].pid) + ' has arrived at time ' + repr(process[i].arrival_time))
+            print('burst ' + repr(process[i].get_burst()))
+    if context_switch == True:
+        switch_counter += 1
+        sysclock += 1
+        if switch_counter > 5:
+            context_switch = False
+            switch_counter = 1
+    else:
+        try: 
+            running_process = ready_queue.get(False)
+        except Empty:
+            pass
 
-    while True:
-        time_quantum -= 1
-        burst_time -= 1
+        if running_process.burst_times:
+            burst_time = running_process.get_burst()
 
-        if burst_time == 0:
-            wait_queue.put(running_process)
-            sysclock += 5
-            print('test')
-            burst_time = 4
+        while True:
+            time_quantum -= 1
+            burst_time -= 1
 
+            if burst_time == 0:
+                wait_queue.put(running_process)
+                context_switch = True
+                running_process = wait_queue.get()
+                terminated += 1
+                break
+            elif time_quantum == 0:
+                ready_queue.put(running_process)
+                running_process.burst_times.appendleft(burst_time)
+                running_process = ready_queue.get()
+                time_quantum = 5
+                context_switch = True
+
+        if running_process.burst_times:
+            print('burst_time ' + repr(running_process.get_burst()))
+        else:
+            print('burst empty')
+        if terminated >= number_of_processes:
+            print(terminated)
             break
-        elif time_quantum == 0:
-            ready_queue.put(running_process)
-            running_process.burst_times.appendleft(burst_time)
-            time_quantum = 5
-            sysclock += 5
-            break
-
     sysclock += 1
     j += 1
-    print(sysclock)
+    if j == 1000:
+        break
